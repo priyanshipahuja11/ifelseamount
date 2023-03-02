b75143c13278357a29cd5db84444f99724da7f7a
priyanshipahuja11 <priyanshipahuja11@gmail.com>
2023-03-02 13:54:39 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// var a, b, c;
+// a = 5000;
+// b = prompt("enter any value");
+// c = a * b / 100;
+// document.write(c);
+// if (c % 8 == 0) {
+//     console.log(" increament")
+// }
+// else {
+//     console.log("no increement");
+// }
+
+// var a, b, c;
+// a = prompt("enter any value");
+// for (b = 1; b <= 10; b++) {
+//     c = a * b;
+//     if (c % 2 !== 0) {
+//         document.write(c);
+//     }
+
+// }
+
+var a, b, c;
+a = 340; b = 560; c = 8;
+if (a > b) {
+    if (a > c) {
+        console.log("a is greater");
+    }
+    else {
+        console.log("c is greater");
+    }
+
+}
+else {
+    if (b > c) {
+        console.log("b is greater");
+    }
+    else {
+        console.log("c is greater");
+    }
+}
+
+
+
+
+
+
